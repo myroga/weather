755f28bea6d2383336c5da42b0d0ecdc27f96ea2
niklas_joensson <niklas_joensson@hotmail.com>
2018-07-24 14:57:29 +0200

    Lagt till Daglista

---
+++ src/components/Daglista.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Daglista extends Component {
+  render() {
+    return (
+    <div>
+        <div>
+            <p>
+              Hej!
+            </p>
+        </div> 
+        <div>
+            <p>
+              Hopp!
+            </p>
+        </div> 
+    </div>
+    );
+  }
+}
+
+export default Daglista;
